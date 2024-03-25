1c8c0df40e7840a0d8c1e058c689886b627b029d
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-25 01:40:38 -0700

    Update main.dart with BlocProvider for WeatherCubit
    Replace ChangeNotifierProvider with BlocProvider for WeatherCubit in main.dart
    to manage weather data using BLoC pattern. Also import necessary files for
    WeatherCubit and WeatherService.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
 import 'package:weather_app/pages/home_page.dart';
 import 'package:weather_app/providers/weather_provider.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-     create: (context) {
-        return WeatherProvider();
+  runApp(BlocProvider(
+      create: (context) {
+        return WeatherCubit(WeatherService());
       },
-    child: WeatherApp()));
+      child: WeatherApp()));
 }
 
 class WeatherApp extends StatelessWidget {
@@ -17,7 +19,11 @@ class WeatherApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-      primarySwatch: Provider.of<WeatherProvider>(context).weatherData == null ?  Colors.blue : Provider.of<WeatherProvider>(context).weatherData!.getThemeColor()  ,
+        primarySwatch: Provider.of<WeatherProvider>(context).weatherData == null
+            ? Colors.blue
+            : Provider.of<WeatherProvider>(context)
+                .weatherData!
+                .getThemeColor(),
       ),
       home: HomePage(),
     );

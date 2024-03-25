ce76c3b7b63f927813d33adc56c463360e090151
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-25 01:21:53 -0700

    Add getWeather method to WeatherCubit
    Introduce getWeather method in WeatherCubit to fetch weather data based on
    city name using WeatherService. This method handles loading, success, and
    failure states accordingly.

--- lib/cubits/weather_cubit/weather_cubit.dart
+++ lib/cubits/weather_cubit/weather_cubit.dart
@@ -1,6 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/weather_cubit/weather_state.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherLoading());
+  WeatherCubit(this.weatherService) : super(WeatherLoading());
+
+  WeatherService weatherService;
+  void getWeather({required String cityName}) async {
+    WeatherModel? weatherModel;
+    emit(WeatherLoading());
+    try {
+      weatherModel = await weatherService.getWeather(cityName: cityName);
+      emit(WeatherSuccess());
+    } on Exception catch (e) {
+      emit(WeatherFailure());
+    }
+  }
 }
